f7a4abf514e9b9c861f2e21b539ed91c7385383d
Matheus Pino <matheus.pino@hotmail.com>
2023-05-17 16:46:01 -0300

    Primeiro commit

---
+++ main.js
@@ -0,0 +1,38 @@
+function tocarSom(idElemento){
+
+    const elemento = document.querySelector(idElemento);
+
+    if(elemento != null && elemento.localName === 'audio'){
+        elemento.play();
+    } 
+    else{
+        console.log('Elemento não encontrado ou seletor inválido')
+    }
+    
+};
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+for(let i = 0; i < listaDeTeclas.length; i++){
+
+    const tecla = listaDeTeclas[i];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`
+
+    tecla.onclick = function(){
+        tocarSom(idAudio);
+    };
+
+    tecla.onkeydown = function(evento){
+        if (evento.code === "Space" || evento.code === "Enter"){
+            tecla.classList.add('ativa');
+        }
+    }
+
+    tecla.onkeyup = function(evento){ 
+        if (evento.code === "Space" || evento.code === "Enter"){
+            tecla.classList.remove('ativa');
+        }
+    }
+
+}
